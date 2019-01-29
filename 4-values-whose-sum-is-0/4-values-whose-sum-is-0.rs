64b83cd413baccf9efa41cc08bff3e947680c8ec
Seong Yong-ju <sei40kr@gmail.com>
2019-01-29 23:14:55 +0900

    Add 4-values-whose-sum-is-0

---
+++ 4-values-whose-sum-is-0/4-values-whose-sum-is-0.rs
@@ -0,0 +1,150 @@
+#[allow(unused_imports)]
+use std::cmp::Ordering::{Equal, Greater, Less};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+// 4-values-whose-sum-is-0.rs --- 4 Values whose Sum is 0
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+// tanakah's input macro
+// cf https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+    }
+}
+
+use std::cmp::Ordering;
+
+/// Equivalent to std::lowerbound and std::upperbound in c++
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, &T) -> usize;
+    fn upper_bound(&self, &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+
+        low
+    }
+}
+
+fn main() {
+    input! {
+        n: usize,
+        ps: [i64; n],
+        qs: [i64; n],
+        rs: [i64; n],
+        ss: [i64; n],
+    };
+
+    let mut rss = vec![0; n * n];
+
+    for (i, r) in rs.into_iter().enumerate() {
+        for (j, s) in ss.iter().enumerate() {
+            rss[i * n + j] = r + s;
+        }
+    }
+
+    rss.sort();
+
+    let mut ans = 0;
+
+    for p in ps.into_iter() {
+        for q in qs.iter() {
+            let rs = -p - q;
+            ans += rss.upper_bound(&rs) - rss.lower_bound(&rs);
+        }
+    }
+
+    println!("{}", ans);
+}
